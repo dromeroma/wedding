f35b7f84f7ab36c8809841acdf8aa22f96853936
dromeroma <deimerromeromadera@gmail.com>
2024-02-05 16:51:38 -0500

    Ajustes a formulario

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,17 +4,17 @@ const FormularioGoogle = () => {
   const formularioSrc = "https://docs.google.com/forms/d/e/1FAIpQLSe_c85VIsNY6ND1Y1OhtZsHbxnUvuR5716etrvYAvH7NW9GwA/viewform?embedded=true";
 
   return (
-    <div className="pt-3 pt-md-5">
-      <p className='text-center text-white m-0 fw-bold fs-1'>Formulario de asistencia</p>
-      <div className="container embed-responsive">
+    <div className="container-fluid pt-3 pt-md-5">
+      <p className='text-center text-white fw-bold fs-1'>Formulario de asistencia</p>
+      <div className="d-flex justify-content-center vh-100">
         <iframe
           title="Formulario de Google"
-          className="embed-responsive-item"
+          className="w-100 mw-60"
           src={formularioSrc}
-          // frameBorder="0"
-          // marginHeight="0"
-          // marginWidth="0"
-          // scrolling="no"
+          //frameBorder="0"
+          // marginHeight="600"
+          // marginWidth="400"
+          scrolling="no"
         >
           Cargando...
         </iframe>
