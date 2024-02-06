8d76edffc8a044ea76c5ee0f04f68ef237cbd078
dromeroma <deimerromeromadera@gmail.com>
2024-02-06 13:59:10 -0500

    Animación agregada a la imagen Toca Aquí

--- src/components/Ticket/index.js
+++ src/components/Ticket/index.js
@@ -3,7 +3,7 @@ import 'animate.css'; // Asegúrate de importar animate.css
 
 const TicketImagen = () => {
 
-
+    const [animacionActiva, setAnimacionActiva] = useState(false);
     const obtenerClaseAnimacionAleatoria = () => {
 
         const animacionesDisponibles = [
@@ -51,8 +51,24 @@ const TicketImagen = () => {
         return () => clearInterval(intervalo);
     }, [contadorSegundos]);
 
+    useEffect(() => {
+        // Función para activar y desactivar la animación
+        const toggleAnimation = () => {
+            setAnimacionActiva(true);
+            setTimeout(() => {
+                setAnimacionActiva(false);
+            }, 1000);
+        };
+
+        // Activar la animación cada 3 segundos
+        const interval = setInterval(toggleAnimation, 3000);
+
+        // Limpiar el intervalo cuando el componente se desmonta
+        return () => clearInterval(interval);
+    }, []); // Ejecutar solo una vez al montar el componente
+
     const anchoEstilo = window.innerWidth > 1024 ? '30%' : window.innerWidth < 768 ? '60%' : '50%';
-    const anchoEstilo2 = window.innerWidth > 1024 ? '10%' : window.innerWidth < 768 ? '30%' : '10%';
+    const anchoEstilo2 = window.innerWidth > 1024 ? '15%' : window.innerWidth < 768 ? '30%' : '10%';
 
     return (
         <div className='pt-3 pt-md-5'>
@@ -64,23 +80,22 @@ const TicketImagen = () => {
                 />
             </div>
 
-            <div className='container' style={{ width: anchoEstilo2 }}>
-                <img
-                    src="images/toca-para-interactuar.png" // Reemplaza con la ruta de tu imagen
-                    alt="Toca aquí"
-                    className="img-fluid text-end"
-                />
-            </div>
-
             <div
-                className={`card mx-auto img-fluid animate__animated ${animacionActual}`}
-                style={{ width: '90%' }}
+                className={`card mx-auto img-fluid`}
+                style={{ width: '90%', position: 'relative', backgroundColor: 'transparent' }}
                 onClick={cambiarImagen}
             >
                 <img
-                    className="card-img-top img-fluid rounded-5"
+                    className={`card-img-top img-fluid rounded-5 animate__animated ${animacionActual}`}
                     src={`images/imagen${imagenActual}.png`}
                     alt={`Imagen ${imagenActual}`}
+                />
+
+                <img
+                    src="images/toca-para-interactuar.png" // Reemplaza con la ruta de tu imagen
+                    alt="Toca aquí"
+                    className={`img-fluid text-end animate__animated ${animacionActiva ? 'animate__heartBeat' : ''}`}
+                    style={{ width: anchoEstilo2, position: 'absolute', top: 30, right: 15 }}
                 />
                 {/* <div className="card-body">
             <p className="card-text">Descripción de la imagen o cualquier otro contenido aquí.</p>
